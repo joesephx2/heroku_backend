d22ab204bd6c82beb6224019cd92bfc46e9be623
joesephx2 <58317642+joesephx2@users.noreply.github.com>
2021-07-20 15:37:21 -0600

    updated knexfile & package.json updated

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+const connectionString = process.env.DATABASE_URL;
 
 module.exports = {
 
@@ -6,7 +7,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      connectionString: 'postgres://ieenzhhdvqzjpp:130b66ad17f28205e1889730a4619593d611e5ee27bea8f667273226c767d138@ec2-52-6-77-239.compute-1.amazonaws.com:5432/d6j420n8eu1vjg',
+      connectionString,
       ssl: { rejectUnauthorized: false }
     }
   },
@@ -30,7 +31,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      connectionString: 'postgres://ieenzhhdvqzjpp:130b66ad17f28205e1889730a4619593d611e5ee27bea8f667273226c767d138@ec2-52-6-77-239.compute-1.amazonaws.com:5432/d6j420n8eu1vjg',
+      connectionString,
       ssl: { rejectUnauthorized: false }
     }
 
